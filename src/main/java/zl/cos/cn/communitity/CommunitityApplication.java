897f96771fb1d42c0a9ca4ed78c5de06865a0b0e
zlcoder <599660411@qq.com>
2019-07-18 14:00:11 +0800

    first commit

---
+++ src/main/java/zl/cos/cn/communitity/CommunitityApplication.java
@@ -0,0 +1,14 @@
+package zl.cos.cn.communitity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitityApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CommunitityApplication.class, args);
+    }
+
+}
